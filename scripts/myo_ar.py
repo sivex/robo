075cab47665b3150ce4e7e62aca84cf439a2a286
Zachary Stewart <zachary@zstewart.com>
2015-11-05 18:05:02 -0500

    added threshold motion

--- scripts/myo_ar.py
+++ scripts/myo_ar.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python2
 from __future__ import print_function
 
+import math
 import rospy
 
 import tf
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Imu
+
+def clamp(n, mn, mx):
+    return min(max(n, mn), mx)
 
 def main():
     rospy.init_node('myo_ar', anonymous=True)
@@ -17,13 +21,24 @@ def main():
     def myo_msg_handle(imu):
         ori = imu.orientation
 
-        print(ori)
         x,y,z = tf.transformations.euler_from_quaternion(
             (ori.x, ori.y, ori.z, ori.w))
 
         print(x, y, z)
 
-        arPub.publish(Twist(Vector3(x, y, z), Vector3(0,0,0)))
+        if abs(y) > math.pi / 4.0:
+            f = 1 if y > 0 else -1
+        else:
+            f = 0
+
+        if abs(z) > math.pi / 5.0:
+            h = 1 if z > 0 else -1
+        else:
+            h = 0
+        #f = clamp(y / (math.pi / 4.0), -1.0, 1.0)
+        #h = clamp(z / (math.pi / 4.0), -1.0, 1.0)
+
+        arPub.publish(Twist(Vector3(f, h, 0), Vector3(0,0,0)))
 
     rospy.Subscriber("myo_imu", Imu, myo_msg_handle)
 
