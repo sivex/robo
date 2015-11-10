a0e7cc2604933f5a6779111bef2e9281eb44d8a6
Zachary Stewart <zachary@zstewart.com>
2015-11-10 18:57:08 -0500

    added more stuff

--- scripts/myo_ar.py
+++ scripts/myo_ar.py
@@ -7,9 +7,26 @@ import rospy
 import tf
 from geometry_msgs.msg import Twist, Vector3
 from sensor_msgs.msg import Imu
+from ros_myo import EmgArray
+
+rotdat = [0.0, 0.0, 0.0]
+emgdat = [0.0] * 8
 
 def clamp(n, mn, mx):
     return min(max(n, mn), mx)
+
+def imu_handler(imu):
+    ori = imu.orientation
+
+    x,y,z = tf.transformations.euler_from_quaternion(
+        (ori.x, ori.y, ori.z, ori.w))
+
+    rotdata = [x,y,z]
+
+def emg_handler(emg):
+    emgdat[:] = emg.data[:]
+
+def emg_data(emg)
 
 def main():
     rospy.init_node('myo_ar', anonymous=True)
@@ -17,30 +34,25 @@ def main():
     # Publish to AR drone cmd_vel
     arPub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
-    # handler for myo_imu subscriber.
-    def myo_msg_handle(imu):
-        ori = imu.orientation
-
-        x,y,z = tf.transformations.euler_from_quaternion(
-            (ori.x, ori.y, ori.z, ori.w))
+    rospy.Subscriber("myo_imu", Imu, imu_handler)
+    
+    while True:
+        x,y,z = rotdat
 
         print(x, y, z)
 
-        if abs(y) > math.pi / 4.0:
+        if abs(y) > math.pi / 6.0:
             f = 1 if y > 0 else -1
         else:
             f = 0
 
-        if abs(z) > math.pi / 5.0:
+        if abs(z) > math.pi / 6.0:
             h = 1 if z > 0 else -1
         else:
             h = 0
-        #f = clamp(y / (math.pi / 4.0), -1.0, 1.0)
-        #h = clamp(z / (math.pi / 4.0), -1.0, 1.0)
 
         arPub.publish(Twist(Vector3(f, h, 0), Vector3(0,0,0)))
 
-    rospy.Subscriber("myo_imu", Imu, myo_msg_handle)
 
     rospy.spin()
 
--- scripts/train_network.py
+++ scripts/train_network.py
@@ -6,7 +6,9 @@ import rospy
 import sys
 import threading
 
-from ros_myo.msg import EmgArray
+import tf
+from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import Imu
 
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
@@ -14,70 +16,77 @@ from pybrain.tools.shortcuts import buildNetwork
 from pybrain.tools.customxml import NetworkWriter, NetworkReader
 
 myo_data_lock = threading.Lock()
-myo_data = [0.0] * 8
+myo_data = (0.0, 0.0, 0.0)
 
-def myo_callback(emgArr):
+joystick_lock = threading.Lock()
+joystick_data = (0.0, 0.0, 0.0)
+
+def myo_callback(imu):
+    global myo_data
+    ori = imu.orientation
+    x,y,z = tf.transformations.euler_from_quaternion(
+        (ori.x, ori.y, ori.z, ori.w))
     with myo_data_lock:
-        myo_data[:] = emgArr.data[:]
+        myo_data = (x, y, z)
+
+def joystick_callback(twist):
+    global joystic_data
+    l = twist.linear
+    with joystick_lock:
+        joystick_data = (l.x, l.y, l.z)
 
 def main():
     rospy.init_node('myo_training', anonymous=True)
 
-    emg_sub = rospy.Subscriber("myo_emg", EmgArray, myo_callback)
-
-    cases = [
-        ("Left", [0, -1]),
-        ("Right", [0, 1]),
-        ("Forward", [1, 0]),
-        ("Backward", [-1, 0]),
-        ("Forward-Left", [1, -1]),
-        ("Forward-Right", [1, 1]),
-        ("Backward-Left", [-1, -1]),
-        ("Backward-Right", [-1, 1]),
-    ]
+    #imu_sub = rospy.Subscriber("myo_imu", Imu, myo_callback)
+    #joy_sub = rospy.Subscriber("cmd_vel", Twist, joystick_callback)
 
     if len(sys.argv[1:]) >= 1 and os.path.exists(sys.argv[1]):
         print "Loading from %s" % sys.argv[1]
         net = NetworkReader.readFrom(sys.argv[1])
     else:
         print "Starting new network"
-        net = buildNetwork(8, 10, 2)
+        net = buildNetwork(3, 3)
 
-    trainSet = SupervisedDataSet(8, 2)
+    #trainSet = SupervisedDataSet(3, 3)
 
-    print "Beginning Data Collection"
+    #rate = rospy.Rate(10)
 
-    for i in xrange(1):
-        random.shuffle(cases)
-        for name, output in cases:
-            print "Go to position %s" % name
-            print "press enter to continue...",
-            raw_input()
+    #print "Press enter to start collection.",
+    #raw_input() 
 
-            with myo_data_lock:
-                print "Snapshotting data: %s" % myo_data
-                trainSet.addSample(myo_data, output)
-            
-    emg_sub.unregister()
+    #print "Beginning Data Collection"
 
-    print "Starting Training"
+    #for i in range(1200):
+    #    with myo_data_lock, joystick_lock:
+    #        trainSet.addSample(myo_data, joystick_data)
+    #        print "Added", myo_data, joystick_data
+    #    rate.sleep()
 
-    trainer = BackpropTrainer(net, trainSet)
+    #imu_sub.unregister()
+    #joy_sub.unregister()
 
-    trainer.trainUntilConvergence()
+    #print "Starting Training"
 
-    print "Training Completed"
+    #trainer = BackpropTrainer(net, trainSet)
 
-    if len(sys.argv[1:]) >= 1:
-        print "Saving trained network in %s" % sys.argv[1]
-        NetworkWriter.writeToFile(net, sys.argv[1])
+    #trainer.trainUntilConvergence()
+
+    #print "Training Completed"
+
+    #if len(sys.argv[1:]) >= 1:
+    #    print "Saving trained network in %s" % sys.argv[1]
+    #    NetworkWriter.writeToFile(net, sys.argv[1])
 
     print "Running rest of input on network"
 
-    def run_network(emgArr):
-        print net.activate(emgArr.data)
+    def run_network(imu):
+        ori = imu.orientation
+        x,y,z = tf.transformations.euler_from_quaternion(
+            (ori.x, ori.y, ori.z, ori.w))
+        print net.activate((x,y,z))
 
-    emg_sub = rospy.Subscriber("myo_emg", EmgArray, run_network)
+    imu_sub = rospy.Subscriber("myo_imu", Imu, run_network)
 
     rospy.spin()
 
